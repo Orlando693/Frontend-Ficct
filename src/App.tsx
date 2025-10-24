6f73ba90c45900e9ba8f46c37c15a4dec75677d8
Orlando693 <orlandosebastian693@gmail.com>
2025-10-24 19:20:14 -0400

    Se configuro el entorno de trabajo con React y Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main className="fixed inset-0 grid place-items-center bg-neutral-900">
+      <h1 className="text-3xl font-bold bg-indigo-600 text-white px-4 py-2 rounded-xl shadow">
+        Tailwind OK ✅
+      </h1>
+    </main>
+  )
+}
